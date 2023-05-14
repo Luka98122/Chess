0371f00a19aae6cfe3d84af4ae12e61ce3f88640
Dexxxon09 <63368484+Dexxxon09@users.noreply.github.com>
2023-05-14 15:39:39 +0200

    Added pieces
    Added bishop, Queen, King, fixed various bugs, pawns can move tow spaces on start, and board starts with most pieces. ( These recent pushes were done together) @luka98122 and @Dexxxon09

--- Main.py
+++ Main.py
@@ -10,6 +10,7 @@ pygame.display.set_caption("Chess.com")
 class Pawn:
     # init
     def __init__(self, x, y, color):
+        self.moves = 0
         self.x = x
         self.y = y
         self.color = color
@@ -17,6 +18,7 @@ class Pawn:
     def move(self, coords):
         self.x = coords[0]
         self.y = coords[1]
+        self.moves +=1
 
     def draw(self, window):
         if self.color == 0:
@@ -31,10 +33,12 @@ class Rook:
         self.x = x
         self.y = y
         self.color = color
+        self.moves=0
 
     def move(self, coords):
         self.x = coords[0]
         self.y = coords[1]
+        self.moves+=1
 
     def draw(self, prozor):
         if self.color == 0:
@@ -58,10 +62,43 @@ class Bishop:
     def draw(self, prozor):
         if self.color == 0:
             col = pygame.Color("Black")
-
         else:
             col = pygame.Color("Red")
         pygame.draw.rect(prozor, col, (40 + self.x * 100, 20 + self.y * 100, 20, 50))
+
+class Queen:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def move(self, coords):
+        self.x = coords[0]
+        self.y = coords[1]
+
+    def draw(self, prozor):
+        if self.color == 0:
+            col = pygame.Color("Black")
+        else:
+            col = pygame.Color("Red")
+        pygame.draw.rect(prozor, col, (20 + self.x * 100, 20 + self.y * 100, 50, 50))
+
+class King:
+    def __init__(self, x, y, color):
+        self.x = x
+        self.y = y
+        self.color = color
+
+    def move(self, coords):
+        self.x = coords[0]
+        self.y = coords[1]
+
+    def draw(self, prozor):
+        if self.color == 0:
+            col = pygame.Color("Blue")
+        else:
+            col = pygame.Color("Green")
+        pygame.draw.rect(prozor, col, (20 + self.x * 100, 20 + self.y * 100, 50, 50))
 
 
 # Draw function
@@ -126,6 +163,8 @@ def possibleSpots(entity):
             and spotOccupied(entity.x - 1, entity.y + direction)[1] != None
         ):
             spots.append([entity.x - 1, entity.y+direction])
+        if entity.moves == 0:
+            spots.append([entity.x, entity.y + direction*2])
     if type(entity) == Rook:
         spots = []
         dirs = [[0,1],[0,-1],[1,0],[-1,0]]
@@ -145,8 +184,60 @@ def possibleSpots(entity):
                             break
                         else:
                             break
-
-
+    if type(entity) == Bishop:
+        dirs = [[1, -1], [1, 1], [-1, 1], [-1, -1]]
+        for i in range(4):
+            dir = dirs[i]
+            for j in range(1, 9):
+                status = spotOccupied(entity.x + dir[0] * j, entity.y + dir[1] * j)
+                if status[0] == False:
+                    spots.append([entity.x + dir[0] * j, entity.y + dir[1] * j])
+                elif status[0] == True:
+                    print("Usao")
+                    if status[1] == None:
+                        break
+                    elif status[1] != None:
+                        if status[1].color != entity.color:
+                            spots.append([entity.x + dir[0] * j, entity.y + dir[1] * j])
+                            break
+                        else:
+                            break
+    if type(entity) == Queen:
+        dirs = [[1, -1], [1, 1], [-1, 1], [-1, -1],[0,1],[0,-1],[1,0],[-1,0]]
+        for i in range(8):
+            dir = dirs[i]
+            for j in range(1, 9):
+                status = spotOccupied(entity.x + dir[0] * j, entity.y + dir[1] * j)
+                if status[0] == False:
+                    spots.append([entity.x + dir[0] * j, entity.y + dir[1] * j])
+                elif status[0] == True:
+                    print("Usao")
+                    if status[1] == None:
+                        break
+                    elif status[1] != None:
+                        if status[1].color != entity.color:
+                            spots.append([entity.x + dir[0] * j, entity.y + dir[1] * j])
+                            break
+                        else:
+                            break
+    if type(entity) == King:
+        dirs = [[1, -1], [1, 1], [-1, 1], [-1, -1],[0,1],[0,-1],[1,0],[-1,0]]
+        for i in range(8):
+            dir = dirs[i]
+            for j in range(1, 2):
+                status = spotOccupied(entity.x + dir[0] * j, entity.y + dir[1] * j)
+                if status[0] == False:
+                    spots.append([entity.x + dir[0] * j, entity.y + dir[1] * j])
+                elif status[0] == True:
+                    print("Usao")
+                    if status[1] == None:
+                        break
+                    elif status[1] != None:
+                        if status[1].color != entity.color:
+                            spots.append([entity.x + dir[0] * j, entity.y + dir[1] * j])
+                            break
+                        else:
+                            break
 
     return spots
 
@@ -167,7 +258,36 @@ def setUpPawns():
 
 def setUpBoard():
     setUpPawns()
-
+    # Rooks
+    rook1 = Rook(0,0,1)
+    rook2 = Rook(7, 0, 1)
+    rook3 = Rook(0,7,0)
+    rook4 = Rook(7, 7, 0)
+    entityList.append(rook1)
+    entityList.append(rook2)
+    entityList.append(rook3)
+    entityList.append(rook4)
+    # Bishops
+    b1 = Bishop(1, 0, 1)
+    b2 = Bishop(6, 0, 1)
+    b3 = Bishop(1, 7, 0)
+    b4 = Bishop(6, 7, 0)
+    entityList.append(b1)
+    entityList.append(b2)
+    entityList.append(b3)
+    entityList.append(b4)
+    # Queens
+    q1 = Queen(3,0,1)
+    q2 = Queen(3, 7, 0)
+    entityList.append(q1)
+    entityList.append(q2)
+    # Kings
+    k1 = King(4, 0, 1)
+    k2 = King(4, 7, 0)
+    entityList.append(k1)
+    entityList.append(k2)
+# COMMENt
+a =2 
 
 setUpBoard()
 # Main
@@ -189,6 +309,10 @@ def main():
             mousePos = pygame.mouse.get_pos()
             tilePos = getTileClickedOn(mousePos)
             entityClickedOn = spotOccupied(tilePos[0], tilePos[1])[1]
+            if entityClickedOn != None and lastEntity == entityClickedOn:
+                entityClickedOn = None
+                lastEntity = None
+                spots = None
             res = spotOccupied(tilePos[0],tilePos[1])
             if spots != None:
                 if lastEntity != None:
